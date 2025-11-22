838672fe17d3a66bad9f5a679576845f9301a217
Alex <jinghaowang1997@gmail.com>
2025-11-22 12:07:01 +0000

    1.7

--- Test.java
+++ Test.java
@@ -1,6 +1,24 @@
 public class Test {
-    
+
+    public static int lowestCommon(long a, long b){
+        for(int pos = 0; pos < 16; pos++){
+            long mask = 1L;
+
+            if(((a & mask) != 0) && ((b & mask) != 0)){
+                return pos;
+            }
+
+            a >>= 1;
+            b >>= 1;
+
+            if(a == 0 || b == 0){
+                break;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Lowest common set bit position of 18 and 20: " + lowestCommon(14, 25));
     }
 }
