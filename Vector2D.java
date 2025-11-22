b2ef696f5f587f9f4a5b48804439af7d336d901f
Alex <jinghaowang1997@gmail.com>
2025-11-22 13:45:20 +0000

    realized I misread instructions

--- Vector2D.java
+++ Vector2D.java
@@ -1,29 +1,31 @@
 public class Vector2D {
-    private double x;
-    private double y;
+    private float x;
+    private float y;
 
-    public Vector2D(double x, double y) {
-        this.x = x;
+    public Vector2D(float x, float y) {
+        this.x = x; 
         this.y = y;
     }
 
-    public double magnitude() {
-        return Math.sqrt(x * x + y * y);
+    public float magnitude() {
+        return (float)Math.sqrt(x * x + y * y);
     }
 
-    public Vector2D add(Vector2D other) {
-        return new Vector2D(this.x + other.x, this.y + other.y);
+    public void add(Vector2D other) {
+        this.x += other.x;
+        this.y += other.y;
     }
 
-    public Vector2D scalarMultiply(double other) {
-        return new Vector2D(this.x * other, this.y * other);
+    public void scalarMultiply(float other) {
+        this.x *= other;
+        this.y *= other;
     }
 
-    public Vector2D normalize() {
-        double mag = magnitude();
-        if (mag == 0) {
-            return new Vector2D(0, 0);
+    public void normalize() {
+        float m = magnitude();
+        if (m != 0) {
+            this.x /= m;
+            this.y /= m;
         }
-        return new Vector2D(this.x / mag, this.y / mag);
     }
 }
